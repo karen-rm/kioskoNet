7dfce1599ce8b5031345c60b7ff790629c24218d
AnaKRM22 <akramirezmtz22@gmail.com>
2025-04-30 23:44:15 -0600

    Definiendo flujo de trabajo

---
+++ frontend/pages/gestionContenido.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Agregar un titulo</h1>
+  <form>
+    <label for="isbn"></label><br>
+    <input type="text" id="isbn" name="isbn" required><br><br>
+  </form>
+</body>
+</html>
